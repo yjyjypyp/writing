22c84284bbc6dbc0dc79ba4eceba8c886a7f6c7b
yjyjypyp <72296918+yjyjypyp@users.noreply.github.com>
2024-12-20 21:04:19 +0900

    Add files via upload

--- js/text/text_ep001.js
+++ js/text/text_ep001.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const koButton = document.getElementById('ko');
     const enButton = document.getElementById('en');
-    const languageSelector = document.getElementById('language-selector');
 
     // 텍스트 및 링크 번역
     const translations = {
         en: {
             description: 'Reflections on Space<br><br>The translation will be updated soon. Sorry for your inconvenience',
-			commentinfo: 'You can check the comments from here',
-			messageInput: 'Write your comments',
-			submitMessage: 'Add',
+            commentinfo: 'You can check the comments from here',
+            messageInput: 'Write your comments',
+            submitMessage: 'Add',
             instagram: '@yejin.wip',
             website: '▫ yejinschoi.com ↗',
         },
@@ -22,8 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
             website: '▫ yejinschoi.com &#x2197;',
         }
     };
-
-    // 언어 선택이 변경되었을 때 텍스트 업데이트
+// 언어 선택이 변경되었을 때 텍스트 업데이트
     koButton.addEventListener('click', function () {
         updateLanguage('ko');
     });
@@ -37,13 +35,13 @@ document.addEventListener('DOMContentLoaded', function () {
         // 번역된 텍스트 적용
         const translation = translations[language];
         document.getElementById('description').innerHTML = translation.description;
-		document.getElementById('commentinfo').innerHTML = translation.commentinfo;
-		document.getElementById('messageInput').innerHTML = translation.messageInput;
-		document.getElementById('submitMessage').innerHTML = translation.submitMessage;
+        document.getElementById('commentinfo').innerHTML = translation.commentinfo;
+        document.getElementById('messageInput').placeholder = translation.messageInput;
+        document.getElementById('submitMessage').innerHTML = translation.submitMessage;
         document.querySelector('p2 a[href="https://www.instagram.com/yejin.wip?igsh=b2xyaHJpeG9zMTlm"]').innerHTML = translation.instagram;
         document.querySelector('p2 a[href="https://yejinschoi.com/"]').innerHTML = translation.website;
-		
-		// 언어에 맞게 lang 속성 업데이트
+
+        // 언어에 맞게 lang 속성 업데이트
         document.documentElement.lang = language;
 
         // 버튼에 active 클래스를 적용해 밑줄 표시
@@ -54,8 +52,12 @@ document.addEventListener('DOMContentLoaded', function () {
             enButton.classList.add('active');
             koButton.classList.remove('active');
         }
+
+        // 언어 설정을 로컬 스토리지에 저장
+        localStorage.setItem('language', language);
     }
 
-    // 초기 로딩 시 한국어로 설정
-    updateLanguage('ko');
+    // 페이지가 로드될 때, 로컬 스토리지에서 언어 값 가져오기
+    const savedLanguage = localStorage.getItem('language') || 'ko'; // 기본값은 'ko'
+    updateLanguage(savedLanguage); // 로컬 스토리지에 저장된 언어로 초기화
 });
