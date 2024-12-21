73f350889f971ea873670324bed27fe2a0fa4b05
yjyjypyp <72296918+yjyjypyp@users.noreply.github.com>
2024-12-21 19:06:55 +0900

    Add files via upload

--- js/imgpopup.js
+++ js/imgpopup.js
@@ -64,6 +64,7 @@ function addImage(imgSrc, title) {
 }
 
  // 최근 이미지를 위에 배치
+addImage('../img/episode_img/100_1811.JPG', 'Lost and found 2, 21.12.24');
 addImage('../img/episode_img/100_1795.JPG', 'Belongings, 21.12.24');
 addImage('../img/episode_img/100_1786.JPG', 'Lost and found, 21.12.24');
 addImage('../img/episode_img/100_1806.JPG', 'Life, 21.12.24');
